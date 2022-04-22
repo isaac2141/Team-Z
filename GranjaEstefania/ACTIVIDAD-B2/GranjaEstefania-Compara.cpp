f8d477a36a40be8fd023361edcda13bfc2f2ecdb
Jeriko7 <estefania.granja.jaramillo@utelvt.edu.ec>
2022-04-22 00:30:40 -0500

    Ejercicio Compara

---
+++ GranjaEstefania/ACTIVIDAD-B2/GranjaEstefania-Compara.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float x,y;
+	if(x==y){
+	cout<<"El valor de X:  "<<x<<" Es igual a Y:  "<<y<< endl;
+	}else{
+	if(x<y){
+	cout<<"El valor de X:  "<<x<<"Es menor a Y:  "<<y<<endl;
+	}else{
+	cout<<"El valor de Y:  "<<y<<"Es menor a X:  "<<x<<endl;
+	}
+	}
+	return 0;
+}
