058e566301d5a73f0c7eaae7829f99b320f568ab
isaac2141 <isaac.gonzalez.sanchez@utelvt.edu.ec>
2022-05-01 23:31:05 -0500

    Actualizacion ACTIVIDAD-B2 desde mi realme 7 pro

--- GonzalezIsaac/ACTIVIDAD-B2/GonzalezIsaac-PuntoVenta.cpp
+++ GonzalezIsaac/ACTIVIDAD-B2/GonzalezIsaac-PuntoVenta.cpp
@@ -3,30 +3,26 @@ using namespace std;
 int main()
 {
 	int C=0,P;
-	float A=0,x,Tb,PIVA,Pdsc,IVA=0.12,dsc=0.25,VT;
+	float A=0,x,Vf,IVA=0.12,dsc,Vb,Vi,Vd;
 	cout<<"Ingrese la cantidad de Productos Comprados:  ";
 	cin>>P;
+	cout<<"Ingrese el % del descuento que se realizarà:  ";
+	cin>>dsc;
 	do{
 	cout<<"Ingrese el Precio del Producto:  $  ";
 	cin>>x;
 	C=C+1;
 	A=A+x;
-	PIVA=A*IVA;
-	cout<<"El valor del IVA de su compra es:  $ "<<PIVA<<endl;
-	Pdsc=A*dsc;
-	cout<<"El valor del Descuento a su compra es:  $ "<<Pdsc<<endl;
-	Tb=PIVA+Pdsc;
-       }while(C<P);
-	if(Tb==A){
-	cout<<"Su compra no aplica a un Descuento  "<<Tb<<endl;
-        IVA=Tb*PIVA/100;
-        dsc=Tb*Pdsc/100;
-	cout<<"Su valor con Descuento es:  $  "<<Pdsc<<endl;
-	Tb=PIVA+Pdsc;
-	}else{
-	VT=A+PIVA-Pdsc;
-	cout<<"El valor Total de su Compra es:  $"<<VT<<endl;
-	}
+	}while(C<P);
+	Vb=A;
+	Vd=A*dsc/100;
+	Vi=A*IVA;
+	Vf=A+Vi-Vd;
+
+        cout<<"El valor bruto de su compra es de:  $ "<<Vb<<endl;
+	cout<<"El valor del IVA de su compra es de:  $ "<<Vi<<endl;
+	cout<<"El valor del Descuento de su compra es de:  $ "<<Vd<<endl;
+	cout<<"El valor final de su compra a pagar es de:  $"<<Vf<<endl;
 	return 0;
 }
 	
