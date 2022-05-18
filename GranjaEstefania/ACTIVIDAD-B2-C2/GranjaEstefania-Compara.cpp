25ed8cb05d2dc1b3166b162f11cfe3dcb9b0bb26
Jeriko7 <estefania.granja.jaramillo@utelvt.edu.ec>
2022-05-17 21:49:57 -0500

    Actualizacion ACTIVIDAD-B2-C2

--- GranjaEstefania/ACTIVIDAD-B2-C2/GranjaEstefania-Compara.cpp
+++ GranjaEstefania/ACTIVIDAD-B2-C2/GranjaEstefania-Compara.cpp
@@ -1,19 +1,35 @@
+//=============================================
+//===> Nombre del Programa:Comparación de 2 numeros
+//===> Archivo:Compara.cpp
+//===>Autor: Granja Jaramillo Estefania Lizbeth
+//===> Fecha de Creacion: 23/04/2022
+//===> Fecha de Actualización: 17/05/2022
+//==============================================
 #include<iostream>
 using namespace std;
 int main()
 {
-	float x,y;
+
+cout<<"//============================================="<<endl;
+cout<<"//===> Nombre del Programa:Comparación de 2 numeros"<<endl;
+cout<<"//===> Archivo:Compara.cpp"<<endl;
+cout<<"//===>Autor: Granja Jaramillo Estefania Lizbeth"<<endl;
+cout<<"//===> Fecha de Creacion: 23/04/2022"<<endl;
+cout<<"//===> Fecha de Actualización: 17/05/2022"<<endl<<endl;
+cout<<"//=============================================="<<endl<<endl;
+
+	float JE_x,JE_y;
 	cout<<"Ingrese valor de X:  ";
-	cin>>x;
+	cin>>JE_x;
 	cout<<"Ingrese valor de Y:  ";
-	cin>>y;
-	if(x==y){
-	cout<<"El valor de X:  "<<x<<"Es igual aY:  "<<y<<endl;
+	cin>>JE_y;
+	if(JE_x==JE_y){
+	cout<<"El valor de X:  "<<JE_x<<"Es igual aY:  "<<JE_y<<endl;
 	}else{
-	if(x<y){
-	cout<<"El valor de X:  "<<x<<"Es menor a Y:  "<<y<<endl;
+	if(JE_x<JE_y){
+	cout<<"El valor de X:  "<<JE_x<<" Es menor a Y:  "<<JE_y<<endl;
 	}else{
-	cout<<"El valor de Y:  "<<y<<"Es menor a X:  "<<x<<endl;
+	cout<<"El valor de Y:  "<<JE_y<<" Es menor a X:  "<<JE_x<<endl;
 	}
 	}
 	return 0;
