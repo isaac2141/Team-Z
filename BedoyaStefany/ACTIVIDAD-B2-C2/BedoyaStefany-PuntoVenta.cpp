ffe2e2ffe2146f7231b4376e7b6ab82ab5495362
Mishelle8 <mishellebedoyanapa@gmail.com>
2022-05-18 17:36:05 -0500

    Actualizacion ACTIVIDAD-B2-C2

--- BedoyaStefany/ACTIVIDAD-B2-C2/BedoyaStefany-PuntoVenta.cpp
+++ BedoyaStefany/ACTIVIDAD-B2-C2/BedoyaStefany-PuntoVenta.cpp
@@ -1,31 +1,44 @@
+//================================================
+// ===> Nombre del Programa:calculo de Iva y Descuento
+// ===> Archivo:PuntoVenta.cpp
+// ===> Autor:Bedoya Napa Stefany Mishelle 
+// ===> Fecha de elaboración:30/04/2022
+// ===> Fecha de actualizacion:18/05/2022
+//=====================================================
 #include<iostream>
 using namespace std;
 int main()
 {
-	int C=0,P;
-	float A=0,x,Tb,PIVA,Pdsc,IVA=0.10,dsc=0.20,VT;
+cout<<"//=========================================="<<endl;
+cout<<"// Nombre del Programa:Calculo de Iva y Descuento"<<endl;
+cout<<"// Archivo:PuntoVenta.cpp"<<endl;
+cout<<"// Autor:BedoyaNapaStefanyMishelle"<<endl;
+cout<<"// Fecha de elaboracion:30/04/2022"<<endl;
+cout<<"//=========================================="<<endl;
+	int SN_C=0,SN_P;
+	float SN_A=0,SN_x,SN_Tb,SN_PIVA,SN_Pdsc,SN_IVA=0.12,SN_dsc=0.25,SN_VT;
 	cout<<"Ingrese la cantidad de Productos Comprados: ";
-	cin>>P;
+	cin>>SN_P;
 	do{
 		cout<<"Ingrese el Precio del Producto: $ ";
-		cin>>x;
-		C=C+1;
-		A=A+x;
-		PIVA=A*IVA;
-		cout<<"El valor del IVA de su compra es: $ "<<PIVA<<endl;
-		Pdsc=A*dsc;
-		cout<<"El valor del Descuento a su compra es: $ "<<Pdsc<<endl;
-		Tb=PIVA+Pdsc;
-	}while(C<P);
-	if(Tb==A){
-		cout<<"Su compra no aplica a un Descuento: "<<Tb<<endl;
-		IVA=Tb*PIVA/100;
-		dsc=Tb*Pdsc/100;
-		cout<<"Su valor con Descuento es: $"<<Pdsc<<endl;
-		Tb=PIVA+Pdsc;
+		cin>>SN_x;
+		SN_C=SN_C+1;
+		SN_A=SN_A+SN_x;
+		SN_PIVA=SN_A*SN_IVA;
+		cout<<"El valor del IVA de su compra es: $ "<<SN_PIVA<<endl;
+		SN_Pdsc=SN_A*SN_dsc;
+		cout<<"El valor del Descuento a su compra es: $ "<<SN_Pdsc<<endl;
+		SN_Tb=SN_PIVA+SN_Pdsc;
+	}while(SN_C<SN_P);
+	if(SN_Tb==SN_A){
+		cout<<"Su compra no aplica a un Descuento: "<<SN_Tb<<endl;
+		SN_IVA=SN_Tb*SN_PIVA/100;
+		SN_dsc=SN_Tb*SN_Pdsc/100;
+		cout<<"Su valor con Descuento es: $"<<SN_Pdsc<<endl;
+		SN_Tb=SN_PIVA+SN_Pdsc;
 	}else{
-		VT=A+PIVA-Pdsc;
-		cout<<"El valor Total de su Compra es: $"<<VT<<endl;
+		SN_VT=SN_A+SN_PIVA-SN_Pdsc;
+		cout<<"El valor Total de su Compra es: $"<<SN_VT<<endl;
 	}
 	return 0;
 }
