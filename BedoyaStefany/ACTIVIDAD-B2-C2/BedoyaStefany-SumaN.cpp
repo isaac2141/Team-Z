33ae3ee021138f67704670eca64fa6e7fe346752
Mishelle8 <mishellebedoyanapa@gmail.com>
2022-05-18 18:22:28 -0500

    Actualizacion ACTIVIDAD-B2-C2

--- BedoyaStefany/ACTIVIDAD-B2-C2/BedoyaStefany-SumaN.cpp
+++ BedoyaStefany/ACTIVIDAD-B2-C2/BedoyaStefany-SumaN.cpp
@@ -1,17 +1,31 @@
+//==================================================
+//===> Nombre del Programa:Suma de números
+//===> Archivo:SumaN.cpp
+//===> Autor:Bedoya Napa Stefany Mishelle
+//===> Fecha de Elaboracion: 30/04/2022
+//===> Fecha de Actualización: 18/05/2022
+
 #include<iostream>
 using namespace std;
 int main()
 {
-	int c=0,a;
-	float s=0,y;
+cout<<"//==============================================="<<endl;
+cout<<"//===> Nombe del Programa:Suma de numeros"<<endl;
+cout<<"//===> Archivo:SumaN.cpp"<<endl;
+cout<<"//===> Autor:Bedoya Napa Stefany Mishelle"<<endl;
+cout<<"//===> Fecha de Elaboracion:30/04/2022"<<endl;
+cout<<"//===> Fecha de Actualizacion:18/05/2022"<<endl;
+cout<<"//==============================================="<<endl<<endl;
+	int SN_C=0,SN_b;
+	float SN_S=0,SN_x;
 	cout<<"Ingrese la cantidad de numeros a sumar: ";
-	cin>>a;
+	cin>>SN_b;
 	do{
 		cout<<"Ingrese un numero: ";
-		cin>>y;
-		c=c+1;
-		s=s+y;
-	} while (c<a);
-	cout<<"La suma total es: "<<s<<endl;
+		cin>>SN_x;
+		SN_C=SN_C+1;
+		SN_S=SN_S+SN_x;
+	} while (SN_C<SN_b);
+	cout<<"La suma total es: "<<SN_S<<endl;
 	return 0;
 }
