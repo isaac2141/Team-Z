9c2ab14e0af99f99bfd63a68e4733ea12af19dad
2003Garcia <dayigarcia2003@gmail.com>
2022-05-19 14:54:33 -0500

    Actualizacion ACTIVIDAD-B2-C2

--- GarciaDayana/ACTIVIDAD-B2-C2/GarciaDayana-laedad.cpp
+++ GarciaDayana/ACTIVIDAD-B2-C2/GarciaDayana-laedad.cpp
@@ -1,27 +1,43 @@
+//=================================================
+//===> Nombre del programa:la edad
+//===> Archivo:GarciaDayana-laedad.cpp
+//===> Autor:Garcia Sanchez Dayana Mariby
+//===> Fecha de creacion:29/04/2022
+//===> Fecha de Actualizacion:18/05/2022
+//=================================================
+
 #include<iostream>
 using namespace std;
 int main()
 {
-	int kk,jj,yy,cc1,pp1,yy1,da,la,ca;
+cout<<"//=========================================="<<endl;
+cout<<"//===> Nombre del programa:la edad"<<endl;
+cout<<"//===> Archivo:GarciaDayana-laedad.cpp"<<endl;
+cout<<"//===> Autor:Garcia Sanchez Dayana Mariby"<<endl;
+cout<<"//===> Fecha de creacion:29/04/2022"<<endl;
+cout<<"//===> Fecha de Actualizacion:18/05/2022"<<endl;
+cout<<"//=========================================="<<endl<<endl;
+
+	int Ds_kk,Ds_jj,Ds_yy,Ds_cc1,Ds_pp1,Ds_yy1,Ds_da,Ds_la,Ds_ca;
 	cout<<"Escriba la fecha actual (dia/mes/año): "<<endl;
-	cin>>kk>>jj>>yy;
+	cin>>Ds_kk>>Ds_jj>>Ds_yy;
 	cout<<"Escriba su fecha de nacimiento (dia/mes/año): "<<endl;
-	cin>>cc1>>pp1>>yy1;
-	if (kk<<cc1){
-		kk=kk+30;
-		jj=jj-1;
-		da=kk-cc1;
+	cin>>Ds_cc1>>Ds_pp1>>Ds_yy1;
+	if (Ds_kk<<Ds_cc1){
+		Ds_kk=Ds_kk+30;
+		Ds_jj=Ds_jj-1;
+		Ds_da=Ds_kk-Ds_cc1;
 	}
 
 	else{
-		da=kk-cc1;
+		Ds_da=Ds_kk-Ds_cc1;
 	}
-	if (jj<<pp1){
-		jj=jj+12;
-		yy=yy-1;
-		la=jj-pp1;
+	if (Ds_jj<<Ds_pp1){
+		Ds_jj=Ds_jj+12;
+		Ds_yy=Ds_yy-1;
+		Ds_la=Ds_jj-Ds_pp1;
 	}
-	ca=yy-yy1;
-	cout<<"Tienes"<<ca<<" años, "<<la<<" meses "<<" y "<<da<<" dias. "<<endl;
+	Ds_ca=Ds_yy-Ds_yy1;
+	cout<<"Tienes"<<Ds_ca<<" años, "<<Ds_la<<" meses "<<" y "<<Ds_da<<" dias. "<<endl;
 	return 0;
 }
