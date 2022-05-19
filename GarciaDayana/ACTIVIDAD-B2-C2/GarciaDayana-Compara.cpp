e5aa7bcd879ff62967985508643ce019589761f0
2003Garcia <dayigarcia2003@gmail.com>
2022-05-18 23:40:02 -0500

    Actualizacion ACTIVIDAD-B2-C2

--- GarciaDayana/ACTIVIDAD-B2-C2/GarciaDayana-Compara.cpp
+++ GarciaDayana/ACTIVIDAD-B2-C2/GarciaDayana-Compara.cpp
@@ -1,19 +1,34 @@
+//=================================================
+//===> Nombre del programa:Comparacion de dos numeros
+//===> Archivo:GarciaDayana-Compara.cpp
+//===> Autor:Garcia Sanchez Dayana Mariby
+//===> Fecha de creacion:29/04/2022
+//===> Fecha de Actualizacion:18/05/2022
+//=================================================
+
 #include<iostream>
 using namespace std;
 int main()
 {
-	float x,y;
+cout<<"//========================================="<<endl;
+cout<<"//===> Nombre del programa:Comparacion de dos numeros"<<endl;
+cout<<"//===> Archivo:GarciaDayana-Compara.cpp"<<endl;
+cout<<"//===> Autor:Garcia Sanchez Dayana Mariby"<<endl;
+cout<<"//===> Fecha de Creacion:29/04/2022"<<endl;
+cout<<"//===> Fecha de Actualizacion:18/05/2022"<<endl;
+cout<<"//=================================================="<<endl<<endl;
+        float Ds_x,Ds_y;
 	cout<<"Ingrese valor de X: ";
-	cin>>x;
+	cin>>Ds_x;
 	cout<<"Ingrese valor de Y: ";
-	cin>>y;
-	if(x==y){
-	cout<<"El valor de X: "<<x<<"es igual a Y: "<<y<<endl;
+	cin>>Ds_y;
+	if(Ds_x==Ds_y){
+	cout<<"El valor de X: "<<Ds_x<<"es igual a Y: "<<Ds_y<<endl;
 	}else{
-	if(x<y){
-	cout<<"El valor de X: "<<x<<"es menor a Y: "<<y<<endl;
+	if(Ds_x<Ds_y){
+	cout<<"El valor de X: "<<Ds_x<<"es menor a Y: "<<Ds_y<<endl;
 	}else{
-	cout<<"El valor de Y: "<<y<<"es menor a X: "<<x<<endl;
+	cout<<"El valor de Y: "<<Ds_y<<"es menor a X: "<<Ds_x<<endl;
 	}
 	}
 	return 0;
