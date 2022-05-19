a264fceda17b9e46b728213af8eb9e54441b4858
2003Garcia <dayigarcia2003@gmail.com>
2022-05-19 01:03:57 -0500

    Actualizacion ACTIVIDAD-B2-C2

--- GarciaDayana/ACTIVIDAD-B2-C2/GarciaDayana-CuentaMoneda.cpp
+++ GarciaDayana/ACTIVIDAD-B2-C2/GarciaDayana-CuentaMoneda.cpp
@@ -1,30 +1,45 @@
+//=================================================
+//===> Nombre del Programa:CuentaMoneda
+//===> Archivo:GarciaDayana-CuentaMoneda.cpp
+//===> Autor:Garcia Sanchez Dayana Mariby
+//===> Fecha de creacion:29/04/2022
+//===> Fecha de Actualizacin:18/05/2022
+//=================================================
+
 #include<iostream>
 using namespace std;
 int main()
 {
-	int n,c=0,c1=0,c2=0;
-	float x,a=0,a1=0,a2=0;
+cout<<"//=========================================="<<endl;
+cout<<"//===> Nombre del Programa:CuentaMoneda"<<endl;
+cout<<"//===> Archivo:GarciaDayana-CuentaMoneda.cpp"<<endl;
+cout<<"//===> Autor:Garcia Sanchez Dayana Mariby"<<endl;
+cout<<"//===> Fecha de creacion:29/04/2022"<<endl;
+cout<<"//===> Fecha de Actualizacion:18/05/2022"<<endl;
+cout<<"//=========================================="<<endl<<endl;
+	int Ds_n,Ds_c=0,Ds_c1=0,Ds_c2=0;
+	float Ds_x,Ds_a=0,Ds_a1=0,Ds_a2=0;
 	cout<<"Cantidad de monedas a ingresar: ";
-	cin>>n;
+	cin>>Ds_n;
 	do{
 		 cout<<"Ingrese el valor de la moneda (0.10,0.25): ";
-		 cin>>x;
-		 c=c+1;
-		 a=a+x;
-		 if(x==(float)0.10){
-		c1=c1+1;
-		a1=a1+x;
+		 cin>>Ds_x;
+		 Ds_c=Ds_c+1;
+		 Ds_a=Ds_a+Ds_x;
+		 if(Ds_x==(float)0.10){
+		Ds_c1=Ds_c1+1;
+		Ds_a1=Ds_a1+Ds_x;
 		 }else{
-		c2=c2+1;
-		a2=a2+x;
+		Ds_c2=Ds_c2+1;
+		Ds_a2=Ds_a2+Ds_x;
 		 }
-	}while(c<n);
+	}while(Ds_c<Ds_n);
 	cout<<"El resultado fue: "<<endl;
-	cout<<"Cantidad de monedas ingresadas: "<<c<<endl;
-	cout<<"Cantidad total en dinero contado: "<<a<<endl;
-	cout<<"Cantidad de monedas de 0.10 ctv ingresadas: "<<c1<<endl;
-	cout<<"Cantidad total en dinero en monedas 0.10: "<<a1<<endl;
-	cout<<"Cantidad de monedas de 0.25ctv ingresadas: "<<c2<<endl;
-	cout<<"Cantidad total en dinero de monedas de 0.25: "<<a2<<endl;
+	cout<<"Cantidad de monedas ingresadas: "<<Ds_c<<endl;
+	cout<<"Cantidad total en dinero contado: "<<Ds_a<<endl;
+	cout<<"Cantidad de monedas de 0.10 ctv ingresadas: "<<Ds_c1<<endl;
+	cout<<"Cantidad total en dinero en monedas 0.10: "<<Ds_a1<<endl;
+	cout<<"Cantidad de monedas de 0.25ctv ingresadas: "<<Ds_c2<<endl;
+	cout<<"Cantidad total en dinero de monedas de 0.25: "<<Ds_a2<<endl;
 	return 0;
 }
