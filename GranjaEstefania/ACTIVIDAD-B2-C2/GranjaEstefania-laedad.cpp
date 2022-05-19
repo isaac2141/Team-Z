5af5fe8c8d346d2c17194d1b75596d779df8988f
Jeriko7 <estefania.granja.jaramillo@utelvt.edu.ec>
2022-05-19 17:10:52 -0500

    Actualizacion ACTIVIDAD-B2-C2

--- GranjaEstefania/ACTIVIDAD-B2-C2/GranjaEstefania-laedad.cpp
+++ GranjaEstefania/ACTIVIDAD-B2-C2/GranjaEstefania-laedad.cpp
@@ -1,29 +1,44 @@
+//=============================================
+//===>Nombre del Programa: Calcular La edad de una Persona
+//===>Archivo:Laedad.cpp
+//===>Autor: Granja Jaramillo Estefania Lizbeth
+//===>Fecha de creacion:23/04/2022
+//===>Fecha de Actualizacion:19/05/2022
+//==============================================
 #include<iostream>
 using namespace std;
 int main()
 {
-	int dd,mm,yy,dd1,mm1,yy1,da,ma,ya;
+
+cout<<"//============================================="<<endl;
+cout<<"//===>Nombre del Programa: Calcular La edad de una Persona"<<endl;
+cout<<"//===>Archivo:Laedad.cpp"<<endl;
+cout<<"//===>Autor: Granja Jaramillo Estefania Lizbeth"<<endl;
+cout<<"//===>Fecha de creacion:23/04/2022"<<endl;
+cout<<"//===>Fecha de Actualizacion:19/05/2022"<<endl;
+cout<<"//=============================================="<<endl<<endl;
+	int JE_dd,JE_mm,JE_yy,JE_dd1,JE_mm1,JE_yy1,JE_da,JE_ma,JE_ya;
 	cout<<"Ingrese la Fecha Actual: Dia Mes Año "<<endl;
-	cin>>dd>>mm>>yy;
+	cin>>JE_dd>>JE_mm>>JE_yy;
 	cout<<"Ingrese la Fecha de Nacimiento: Dia Mes Año "<<endl;
-	cin>>dd1>>mm1>>yy1;
-	if(dd<dd1){
-		dd=dd+30;
-		mm=mm-1;
-		da=dd-dd1;
+	cin>>JE_dd1>>JE_mm1>>JE_yy1;
+	if(JE_dd<JE_dd1){
+		JE_dd=JE_dd+30;
+		JE_mm=JE_mm-1;
+		JE_da=JE_dd-JE_dd1;
 	}
 	else{
-		da=dd-dd1;
+		JE_da=JE_dd-JE_dd1;
 	}
-	if(mm<mm1){
-		mm=mm+12;
-		yy=yy-1;
-		ma=mm-mm1;
+	if(JE_mm<JE_mm1){
+		JE_mm=JE_mm+12;
+		JE_yy=JE_yy-1;
+		JE_ma=JE_mm-JE_mm1;
 	}
 	else{
-		ma=mm-mm1;
+		JE_ma=JE_mm-JE_mm1;
 	}
-	ya=yy-yy1;
-	cout<<"Ustd Tiene: "<<ya<<" años "<<ma<<" meses "<<" y "<<da<<" dias "<<endl;
+	JE_ya=JE_yy-JE_yy1;
+	cout<<"Ustd Tiene: "<<JE_ya<<" años "<<JE_ma<<" meses "<<" y "<<JE_da<<" dias "<<endl;
 	return 0;
 }
